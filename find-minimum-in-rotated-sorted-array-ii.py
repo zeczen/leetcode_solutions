1e760cf7e99d4686040bf0ec204831f587815ea0
Eyal <54035934+zeczen@users.noreply.github.com>
2025-03-25 17:33:01 +0200

    Update find-minimum-in-rotated-sorted-array-ii.py

--- find-minimum-in-rotated-sorted-array-ii.py
+++ find-minimum-in-rotated-sorted-array-ii.py
@@ -1,36 +1,32 @@
 # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        N = len(nums)
-        def foo(a,b):
-            if a +1 >= b: 
-                yield min(nums[a],nums[b])
-                return
-            m = (a+b) // 2
-            while not nums[(m+1) % N] > nums[m] < nums[m-1]:
-                yield nums[m]
-                if nums[m] < nums[b]:
-                    b = m
-                elif nums[m] > nums[b]:
-                    a = m+1
-                else:
-                    if nums[a] != nums[m]:
-                        b = m
-                    else: 
-                        f1 = foo(a,m)
-                        f2 = foo(m,b)
-                        for l1,l2 in zip(f1,f2):
-                            if l1 != l2: break
-                        if l1 < l2: 
-                            yield l1
-                            yield from f1
-                        else:
-                            yield l2 
-                            yield from f2
-                        return
-        
-                m = (a+b) // 2
-    
-            yield nums[m]
-        *_,last = foo(0,N-1)
-        return last
+        def dfs(l,r):
+            while r-l > 2:
+                m = (l+r) // 2
+                # print(l, m, r)
+                if nums[l] < nums[m] < nums[r]:    # 1 2 3
+                    return nums[l]
+                elif nums[r] < nums[l] < nums[m]:  # 2 3 1
+                    l = m+1
+                elif nums[m] < nums[r] < nums[l]:  # 3 1 2
+                    r = m-1
+                    if nums[r] > nums[m]: return nums[m]
+                elif nums[l] == nums[m] < nums[r]: # 1 1 2
+                    r = m-1
+                elif nums[r] == nums[m] < nums[l]: # 2 1 1
+                    r = m-1
+                    if nums[r] > nums[m]: return nums[m]
+                elif nums[l] == nums[r] < nums[m]: # 1 2 1
+                    l = m+1
+                elif nums[m] == nums[r] > nums[l]: # 1 2 2
+                    r = m-1
+                elif nums[l] == nums[r] > nums[m]: # 2 1 2
+                    r = m-1
+                    if nums[r] > nums[m]: return nums[m]
+                elif nums[m] == nums[l] > nums[r]: # 2 2 1
+                    l = m+1
+                elif nums[l] == nums[m] == nums[r]:# 1 1 1
+                    return min(dfs(l,m-1), dfs(m+1,r))
+            return min(nums[l:r+1])
+        return dfs(0, len(nums)-1)
